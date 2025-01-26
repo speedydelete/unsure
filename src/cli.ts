3c52a2d1a279587709f75f730df3d73998e67488
speedydelete <speedykorok777@gmail.com>
2025-01-26 14:33:00 -0500

    add -c option

--- src/cli.ts
+++ src/cli.ts
@@ -5,13 +5,19 @@ import {run} from './interpreter';
 
 program.name('unsure');
 program.description('The unsure programming language');
-program.version('0.2.0');
+program.version('0.2.0', '-v, --version', 'displays the version');
+program.helpOption('-h, --help', 'display help for command');
 
-program.argument('<path>', 'The file path of the program to run');
+program.argument('[path]', 'the file path of the program to run');
+program.option('-c, --code <code>', 'the code to run');
 
 program.parse()
 const options = program.opts();
 
-let code: string;
-code = fs.readFileSync(program.args[0]).toString();
-console.log(run(code));
+if (options.code !== undefined) {
+    console.log(run(options.code));
+} else {
+    const code = fs.readFileSync(program.args[0]).toString();
+    console.log(run(code));
+}
+
